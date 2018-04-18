cc9782bbdad50b7ae5fb03efd0d890f8d7c4a4c4
fengguangke <fengguangke2010@126.com>
2018-04-18 23:58:16 +0800

    添加loginView 路由规则

--- runServer.py
+++ runServer.py
@@ -5,6 +5,7 @@ from flask_restful import Api
 # from .registerView import Register
 from registerView import RegisterView
 from userView import UserView
+from loginView import LoginView
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -13,7 +14,8 @@ api = Api(app)
 
 # register 'Register' Resource
 api.add_resource(RegisterView,'/register',endpoint='register')
-api.add_resource(UserView,'/user','/user/<user_id>')
+api.add_resource(UserView,'/user','/user/<user_id>',endpoint='user')
+api.add_resource(LoginView,'/login',endpoint='login')
 
 if __name__ == '__main__':
     app.run(debug=True)
