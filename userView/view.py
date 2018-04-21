dcb9e0c405316b0ceb0217e9e43c9f89124cb986
fengguangke <fengguangke2010@126.com>
2018-04-21 19:14:46 +0800

    1. update parse rules 2. add post function 3. add delete function

--- userView/view.py
+++ userView/view.py
@@ -1,21 +1,27 @@
 # coding=utf-8
 
-from models import UserModel
+from flask import jsonify,request,session
 from flask_restful import Resource,reqparse
 from models import UserModel
+from commoon import verify_token
 
 parse = reqparse.RequestParser()
 parse.add_argument('username',type=str,location='json',required=False)
+parse.add_argument('realName',type=unicode,location='json',required=False)
 parse.add_argument('age',type=int,location='json',required=False)
-parse.add_argument('sex',type=str,location='json',required=False)
+parse.add_argument('sex',type=unicode,location='json',required=False)
 parse.add_argument('phone',type=str,location='json',required=False)
-parse.add_argument('address',type=str,location='json',required=False)
-parse.add_argument('email',type=str,location='json',required=False)
+parse.add_argument('address',type=unicode,location='json',required=False)
+parse.add_argument('email',type=unicode,location='json',required=False)
 parse.add_argument('qq',type=str,location='json',required=False)
-parse.add_argument('avatar',type=str,location='json',required=False)
+parse.add_argument('avatar',type=unicode,location='json',required=False)
+
+parse_header = reqparse.RequestParser()
+parse.add_argument('token',type=str,location='headers',required=True)
 
 class UserView(Resource):
-    # todo add api auth
+
+    method_decorators = [verify_token]
 
     def get(self,user_id):
         '''
@@ -33,12 +39,27 @@ class UserView(Resource):
     def post(self,user_id):
         '''
         update user by user_id
-        :param user_id:
-        :return:user
         '''
-        # todo update user
         update_data = parse.parse_args()
-        pass
+        name = update_data['username']
+        realName = update_data['realName']
+        age = update_data['age']
+        sex = update_data['sex']
+        phone = update_data['phone']
+        address = update_data['address']
+        email = update_data['email']
+        qq = update_data['qq']
+        avatar = update_data['avatar']
+        user = {'username':name,'realName':realName,'age':age,'sex':sex,'phone':phone,
+                'address':address,'email':email,'qq':qq,'avatar':avatar
+                }
+
+        userModel = UserModel()
+        update_re = userModel.update(user_id=user_id,user_info=user)
+        if update_re:
+            return {'code':200,'msg':u'更新用户信息成功：user_id=%s'%user_id}
+        else:
+            return {'code':1005,'error':u"更新用户信息失败: user_id=%s"%user_id},400
 
     def delete(self,user_id):
         '''
@@ -46,10 +67,12 @@ class UserView(Resource):
         :param user_id:
         :return:
         '''
-        pass
-        # todo
-
-
+        userModel = UserModel()
+        delete_re = userModel.deleteUserById(user_id)
+        if delete_re:
+            return {'code':200,'msg':u'删除用户成功,user_id=%s'%user_id}
+        else:
+            return {'code':1006,'error':u'删除用户信息失败,user_id=%s'%user_id},400
 
 
 
