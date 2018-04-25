cf8f6dca88dcdf58bf21fd34be7cc0e1083a1fdb
fengguangke <fengguangke2010@126.com>
2018-04-26 00:12:10 +0800

    userView/view.py

--- userView/view.py
+++ userView/view.py
@@ -1,9 +1,9 @@
 # coding=utf-8
 
 from flask import jsonify,request,session
-from flask_restful import Resource,reqparse
+from flask_restful import Resource,reqparse,fields
 from models import UserModel
-from commoon import verify_token
+from commoon import verify_token,isAdministrator
 
 parse = reqparse.RequestParser()
 parse.add_argument('username',type=str,location='json',required=False)
@@ -16,26 +16,40 @@ parse.add_argument('email',type=unicode,location='json',required=False)
 parse.add_argument('qq',type=str,location='json',required=False)
 parse.add_argument('avatar',type=unicode,location='json',required=False)
 
-parse_header = reqparse.RequestParser()
-parse.add_argument('token',type=str,location='headers',required=True)
+parse_arg = reqparse.RequestParser()
+parse_arg.add_argument('type',type=str,location='args',choices=('name','id'),required=True)
+parse_arg.add_argument('value',type=str,location='args',required=True)
 
 class UserView(Resource):
 
-    method_decorators = [verify_token]
-
-    def get(self,user_id):
+    # method_decorators = [verify_token]
+    @verify_token
+    def get(self):
         '''
         get user info by user_id
         :param user_id:
         :return: user
         '''
         userModel = UserModel()
-        user = userModel.getUserById(user_id)
-        if user:
-            return {'code':200,'msg':u'获取用户信息成功','user':user}
-        else:
-            return {'code':1002,'error':u'获取用户信息失败'},400
 
+        type_value = parse_arg.parse_args()
+        type = type_value['type']
+        user_id_or_name = type_value['value']
+        if type == 'name':
+            user_id = userModel.getUserIdByName(user_id_or_name)
+            if user_id:
+                return {'code': 200, 'msg': u'获取用户id成功', 'user_id': user_id}
+            else:
+                return {'code': 1004, 'error': u'获取用户id成功失败'}, 400
+        else:
+            user = userModel.getUserById(user_id_or_name)
+            user['id'] = user_id_or_name
+            if user:
+                return {'code': 200, 'msg': u'获取用户信息成功', 'user': user}
+            else:
+                return {'code': 1002, 'error': u'获取用户信息失败'}, 400
+
+    @verify_token
     def post(self,user_id):
         '''
         update user by user_id
@@ -61,6 +75,7 @@ class UserView(Resource):
         else:
             return {'code':1005,'error':u"更新用户信息失败: user_id=%s"%user_id},400
 
+    @isAdministrator
     def delete(self,user_id):
         '''
         delete user by user_id
