ed75cc8e59154b41a8583a5b20e2b6e21456a2f7
fengguangke <fengguangke2010@126.com>
2018-04-26 00:08:52 +0800

    add isAdministrator decorator

--- commoon.py
+++ commoon.py
@@ -18,3 +18,14 @@ def verify_token(fun):
         return jsonify({'code':500,'error':u'服务器错误,session["user"]不存在，请登录'})
     return wrapper
 
+def isAdministrator(fun):
+    def wrapper(*args,**kwargs):
+        token = parse_header.parse_args()['token']
+        if 'user' in session:
+            if token != session['user']:
+                return jsonify({'code': 4002, 'error': u'token认证信息错误'})
+            if token != 'admin':
+                return jsonify({"code": 4003, 'error': u"对不起，您没有此权限!"})
+            return fun(*args,**kwargs)
+        return jsonify({'code': 500, 'error': u'服务器错误,session["user"]不存在，请登录'})
+    return wrapper
