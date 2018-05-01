c1298aff30891a0e279c48df35f81d96833ae929
fengguangke <fengguangke2010@126.com>
2018-05-01 23:53:12 +0800

    runServer.py

--- runServer.py
+++ runServer.py
@@ -6,6 +6,7 @@ from flask_restful import Api
 from registerView import RegisterView
 from userView import UserView
 from loginView import LoginView
+from devicesAuditView import DeviceAuditView
 
 app = Flask(__name__)
 app.config.from_pyfile('config.py')
@@ -14,9 +15,9 @@ api = Api(app)
 
 # register 'Register' Resource
 api.add_resource(RegisterView,'/register',endpoint='register')
-api.add_resource(UserView,'/user','/user/<user_id_or_name>',endpoint='user')
+api.add_resource(UserView,'/user','/user/<user_id>',endpoint='user')
 api.add_resource(LoginView,'/login','/logout',endpoint='login_logout')
-
+api.add_resource(DeviceAuditView,'/deviceAudit','/deviceAudit/<audit_id>',endpoint='deviceAudit')
 
 if __name__ == '__main__':
     app.run(debug=True)
