835302786bc6878f7004abd64ca32e8fd2c9c051
fengguangke <fengguangke2010@126.com>
2018-05-01 23:50:31 +0800

    common config file

--- commoon.py
+++ commoon.py
@@ -3,6 +3,10 @@
 from flask_restful import Resource,reqparse
 from flask import jsonify,session
 
+AUDIT_SAVE = 1
+AUDIT_PENDING = 2
+AUDIT_COMPLETE = 3
+
 parse_header = reqparse.RequestParser()
 parse_header.add_argument('token',type=str,location='headers',required=True,help=u'token认证信息错误')
 
@@ -11,21 +15,21 @@ def verify_token(fun):
         token = parse_header.parse_args()['token']
         # if not token:
         #     return jsonify({'code':4001,'error':u'缺少认证信息token'}),401
-        if 'user' in session:
-            if token != session['user']:
+        if 'currentUser' in session:
+            if token != session['currentUser']:
                 return jsonify({'code':4002,'error':u'token认证信息错误'})
             return fun(*args,**kwargs)
-        return jsonify({'code':500,'error':u'服务器错误,session["user"]不存在，请登录'})
+        return jsonify({'code':500,'error':u'服务器错误,session["currentUser"]不存在，请登录'})
     return wrapper
 
 def isAdministrator(fun):
     def wrapper(*args,**kwargs):
         token = parse_header.parse_args()['token']
-        if 'user' in session:
-            if token != session['user']:
+        if 'currentUser' in session:
+            if token != session['currentUser']:
                 return jsonify({'code': 4002, 'error': u'token认证信息错误'})
             if token != 'admin':
                 return jsonify({"code": 4003, 'error': u"对不起，您没有此权限!"})
             return fun(*args,**kwargs)
-        return jsonify({'code': 500, 'error': u'服务器错误,session["user"]不存在，请登录'})
+        return jsonify({'code': 500, 'error': u'服务器错误,session["currentUser"]不存在，请登录'})
     return wrapper
